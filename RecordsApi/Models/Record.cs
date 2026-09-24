32522fb5e6f773aedf9afea9bf870b184b1f98cc
Prajith <prajith.k19022003@gmail.com>
2026-09-23 19:52:07 -0500

    P1_BITBUCKET_REACT_DOTNETCORE_CAKE_PAKET_MVC_MICRO: Bitbucket + Cake + Paket + Microservices

---
+++ RecordsApi/Models/Record.cs
@@ -0,0 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace RecordsApi.Models;
+
+public class Record
+{
+    public int Id { get; set; }
+
+    [Required]
+    [MaxLength(200)]
+    public string Title { get; set; } = string.Empty;
+
+    [MaxLength(1000)]
+    public string? Description { get; set; }
+
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+}
